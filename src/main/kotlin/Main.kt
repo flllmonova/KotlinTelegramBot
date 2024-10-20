4618bef49b0fdd7359e00f3bc4ab19629eabf40d
flllmonova <flllmonova@yandex.ru>
2024-10-21 02:27:05 +0500

    KTB-04 add data class Word, create dictionary in Main.kt, update file words.txt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,5 +4,13 @@ import java.io.File
 
 fun main() {
     val wordsFile = File("words.txt")
-    wordsFile.forEachLine { println(it) }
+
+    val dictionary: MutableList<Word> = mutableListOf()
+
+    wordsFile.forEachLine {
+        val line = it.split("|")
+        dictionary.add(Word(original = line[0], translate = line[1], correctAnswersCount = line[2].toIntOrNull() ?: 0))
+    }
+
+    dictionary.forEach { println(it.toString()) }
 }
