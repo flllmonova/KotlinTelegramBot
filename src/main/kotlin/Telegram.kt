9ee5b2227cb64fc8a2cdca5dbbde7866b374b95e
flllmonova <flllmonova@yandex.ru>
2024-11-14 01:37:31 +0500

    KTB-15 add method to send message in chat, create class TelegramBotService

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -1,47 +1,23 @@
 package org.example
 
-import java.net.URI
-import java.net.http.HttpClient
-import java.net.http.HttpRequest
-import java.net.http.HttpResponse
-
 fun main(args: Array<String>) {
 
     val botToken: String = args[0]
+    val telegramBotService = TelegramBotService(botToken)
+
     var updateId = 0
 
     while (true) {
         Thread.sleep(2000)
-        val updates: String = getUpdates(botToken, updateId)
+        val updates: String = telegramBotService.getUpdates(updateId)
         println(updates)
 
-        val lastUpdateId = getLastUpdateId(updates)
-        if (lastUpdateId == null) continue else updateId = lastUpdateId + 1
+        val lastUpdateId = telegramBotService.getLastUpdateId(updates)
+        if (lastUpdateId == -1) continue else updateId = lastUpdateId + 1
 
-        println(getMessageText(updates))
+        val chatId = telegramBotService.getChatId(updates)
+
+        val messageText = telegramBotService.getMessageText(updates)
+        if (messageText.equals("Hello", ignoreCase = true)) telegramBotService.sendMessage(chatId)
     }
-}
-
-fun getUpdates(botToken: String, updateId: Int): String {
-    val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates?offset=$updateId"
-    val client: HttpClient = HttpClient.newBuilder().build()
-    val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
-    val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())
-
-    return response.body()
-}
-
-fun getLastUpdateId(updates: String): Int? {
-    val lastUpdateIdRegex = "\"update_id\":(\\d+)".toRegex()
-    val matchResult = lastUpdateIdRegex.find(updates)
-    val groups = matchResult?.groups
-    return groups?.get(1)?.value?.toIntOrNull()
-}
-
-fun getMessageText(updates: String): String {
-    val messageTextRegex: Regex = "\"text\":\"(.+?)\"".toRegex()
-    val matchResult: MatchResult? = messageTextRegex.find(updates)
-    val groups: MatchGroupCollection? = matchResult?.groups
-    val text: String = groups?.get(1)?.value ?: "no new messages"
-    return text
 }
