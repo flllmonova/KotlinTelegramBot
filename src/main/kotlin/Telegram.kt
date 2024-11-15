014095db2cfcfd745264f8af8e347c44db7aea9f
flllmonova <flllmonova@yandex.ru>
2024-11-15 18:57:10 +0500

    KTB-15 refactor code: added a constant and a property, left two methods in class TelegramBotService

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -5,19 +5,45 @@ fun main(args: Array<String>) {
     val botToken: String = args[0]
     val telegramBotService = TelegramBotService(botToken)
 
+    var updates = ""
     var updateId = 0
+    var lastUpdateId = 0
+    var chatId = 0
+    var messageText = ""
 
     while (true) {
         Thread.sleep(2000)
-        val updates: String = telegramBotService.getUpdates(updateId)
+        updates = telegramBotService.getUpdates(updateId)
         println(updates)
 
-        val lastUpdateId = telegramBotService.getLastUpdateId(updates)
+        lastUpdateId = getLastUpdateId(updates)
         if (lastUpdateId == -1) continue else updateId = lastUpdateId + 1
 
-        val chatId = telegramBotService.getChatId(updates)
+        chatId = getChatId(updates)
 
-        val messageText = telegramBotService.getMessageText(updates)
+        messageText = getMessageText(updates)
         if (messageText.equals("Hello", ignoreCase = true)) telegramBotService.sendMessage(chatId)
     }
+}
+
+fun getLastUpdateId(updates: String): Int {
+    val lastUpdateIdRegex = "\"update_id\":(\\d+)".toRegex()
+    val matchResult = lastUpdateIdRegex.find(updates)
+    val groups = matchResult?.groups
+    return groups?.get(1)?.value?.toIntOrNull() ?: -1
+}
+
+fun getMessageText(updates: String): String {
+    val messageTextRegex: Regex = "\"text\":\"(.+?)\"".toRegex()
+    val matchResult: MatchResult? = messageTextRegex.find(updates)
+    val groups: MatchGroupCollection? = matchResult?.groups
+    val text: String = groups?.get(1)?.value ?: "no new messages"
+    return text
+}
+
+fun getChatId(updates: String): Int {
+    val chatIdRegex = "\"id\":(\\d+)".toRegex()
+    val matchResult = chatIdRegex.find(updates)
+    val chatId = matchResult?.groups?.get(1)?.value?.toInt() ?: -1
+    return chatId
 }
--- src/main/kotlin/TelegramBotService.kt
+++ src/main/kotlin/TelegramBotService.kt
@@ -5,43 +5,22 @@ import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
+private const val TELEGRAM_API = "https://api.telegram.org/bot"
+
 class TelegramBotService(private val botToken: String) {
 
+    private val client: HttpClient = HttpClient.newBuilder().build()
+
     fun getUpdates(updateId: Int): String {
-        val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates?offset=$updateId"
-        val client: HttpClient = HttpClient.newBuilder().build()
+        val urlGetUpdates = "$TELEGRAM_API$botToken/getUpdates?offset=$updateId"
         val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
         val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())
-
         return response.body()
-    }
-
-    fun getLastUpdateId(updates: String): Int {
-        val lastUpdateIdRegex = "\"update_id\":(\\d+)".toRegex()
-        val matchResult = lastUpdateIdRegex.find(updates)
-        val groups = matchResult?.groups
-        return groups?.get(1)?.value?.toIntOrNull() ?: -1
-    }
-
-    fun getMessageText(updates: String): String {
-        val messageTextRegex: Regex = "\"text\":\"(.+?)\"".toRegex()
-        val matchResult: MatchResult? = messageTextRegex.find(updates)
-        val groups: MatchGroupCollection? = matchResult?.groups
-        val text: String = groups?.get(1)?.value ?: "no new messages"
-        return text
-    }
-
-    fun getChatId(updates: String): Int {
-        val chatIdRegex = "\"id\":(\\d+)".toRegex()
-        val matchResult = chatIdRegex.find(updates)
-        val chatId = matchResult?.groups?.get(1)?.value?.toInt() ?: -1
-        return chatId
     }
 
     fun sendMessage(chatId: Int) {
         val text = "Hello"
         val urlSendMessage = "https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=$text"
-        val client: HttpClient = HttpClient.newBuilder().build()
         val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlSendMessage)).build()
         client.send(request, HttpResponse.BodyHandlers.ofString())
     }
